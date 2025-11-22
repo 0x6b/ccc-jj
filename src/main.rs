c35aaf031a4984485b052d2bf2baebd5c8d07dc5
kaoru <k@warpnine.io>
2025-11-22 21:31:30 +0900

    fix: lower default logging level to WARN

--- src/main.rs
+++ src/main.rs
@@ -249,7 +249,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing::Level::INFO.into()),
+                .add_directive(tracing::Level::WARN.into()),
         )
         .init();
 
